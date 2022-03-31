ecef2475eee7ceb256b0530bd33e55b1cb7d99b9
Farkas <milangamer54@gmail.com>
2022-03-31 19:36:55 +0200

    Added files

---
+++ index.js
@@ -0,0 +1,85 @@
+import Discord from "discord.js";
+import fetch from "node-fetch";
+import { createRequire } from "module";
+const config = createRequire(import.meta.url)("./config.json");
+
+for (let i = 0; i < config.servers.length; i++) {
+    const client = new Discord.Client({
+        intents: [], presence: {
+            status: "idle",
+            activities: [{
+                name: "Loading..",
+                type: "WATCHING"
+            }]
+        }
+    });
+
+    client.on('ready', () => {
+        console.log(`Connected to ${client.user.tag}`);
+    })
+
+    setInterval(async () => {
+        const response = await (await (fetch(`https://api.battlemetrics.com/servers/${config.servers[i].battlemetrics_serverid}`))).json();
+        if (response['data']['attributes']['status'] === "offline" || response['data']['attributes']['status'] === "dead") {
+            client.user.setPresence({
+                status: "dnd",
+                "activities": [{
+                    name: "Server Offline",
+                    type: "PLAYING"
+                }]
+            });
+        }
+        else if (response['data']['attributes']['details']['rust_queued_players']) {
+            client.user.setPresence({
+                status: "online",
+                "activities": [{
+                    name: `${response['data']['attributes']['players']}/${response['data']['attributes']['maxPlayers']} (${response['data']['attributes']['details']['rust_queued_players']})`,
+                    type: "PLAYING"
+                }]
+            });
+            totalPlayers[i] = response['data']['attributes']['players'] + response['data']['attributes']['details']['rust_queued_players'];
+        }
+        else {
+            client.user.setPresence({
+                status: "online",
+                "activities": [{
+                    name: `${response['data']['attributes']['players']}/${response['data']['attributes']['maxPlayers']}`,
+                    type: "PLAYING"
+                }]
+            });
+            totalPlayers[i] = response['data']['attributes']['players'];
+        }
+    }, config.refresh_interval);
+
+    client.login(config.servers[i].token);
+}
+
+let totalPlayers = [];
+
+const client = new Discord.Client({
+    intents: [], presence: {
+        status: "idle",
+        activities: [{
+            name: "Loading..",
+            type: "WATCHING"
+        }]
+    }
+});
+
+client.on('ready', () => {
+    console.log(`Connected to ${client.user.tag}`);
+})
+
+setInterval(() => {
+    if (totalPlayers.length > 0) {
+        client.user.setPresence({
+            status: "online",
+            "activities": [{
+                name: `${totalPlayers.reduce((a, b) => a + b, 0)} Total Players`,
+                type: "PLAYING"
+            }]
+        });
+    }
+}, config.refresh_interval);
+
+client.login(config.global_pop.token);
