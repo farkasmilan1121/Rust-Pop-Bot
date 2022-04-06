40483d4a647e769d1c1ba40f349da87990fa9909
Farkas <milangamer54@gmail.com>
2022-04-06 18:16:06 +0200

    big

--- index.js
+++ index.js
@@ -1,63 +1,11 @@
 import Discord from "discord.js";
 import fetch from "node-fetch";
 import { createRequire } from "module";
-import { setInterval } from "timers/promises";
+import { setInterval } from 'timers';
 const config = createRequire(import.meta.url)("./config.json");
-let totalPlayers = [];
 let clients = [];
-// ---- Server Bots ----
-/*for (let i = 0; i < config.servers.length; i++) {
-    const client = new Discord.Client({
-        intents: [], presence: {
-            status: "idle",
-            activities: [{
-                name: "Loading..",
-                type: "WATCHING"
-            }]
-        }
-    });
 
-    client.on('ready', () => {
-        console.log(`Connected to ${client.user.tag}`);
-    })
-
-    setInterval(async () => {
-        const response = await (await (fetch(`https://api.battlemetrics.com/servers/${config.servers[i].battlemetrics_serverid}`))).json();
-        if (response['data']['attributes']['status'] === "offline" || response['data']['attributes']['status'] === "dead") {
-            client.user.setPresence({
-                status: "dnd",
-                "activities": [{
-                    name: "Server Offline",
-                    type: "PLAYING"
-                }]
-            });
-        }
-        else if (response['data']['attributes']['details']['rust_queued_players']) {
-            client.user.setPresence({
-                status: "online",
-                "activities": [{
-                    name: `${response['data']['attributes']['players']}/${response['data']['attributes']['maxPlayers']} (${response['data']['attributes']['details']['rust_queued_players']})`,
-                    type: "PLAYING"
-                }]
-            });
-            totalPlayers[i] = response['data']['attributes']['players'] + response['data']['attributes']['details']['rust_queued_players'];
-        }
-        else {
-            client.user.setPresence({
-                status: "online",
-                "activities": [{
-                    name: `${response['data']['attributes']['players']}/${response['data']['attributes']['maxPlayers']}`,
-                    type: "PLAYING"
-                }]
-            });
-            totalPlayers[i] = response['data']['attributes']['players'];
-        }
-    }, config.refresh_interval);
-
-    client.login(config.servers[i].token);
-}*/
-
-
+//Connecting to server bots
 for (let i = 0; i < config.servers.length; i++) {
     const client = new Discord.Client({
         intents: [], presence: {
@@ -72,11 +20,11 @@ for (let i = 0; i < config.servers.length; i++) {
         console.log(`Connected to ${client.user.tag}`);
         clients.push(client);
     })
-    client.login(config.servers[i].token); 
+    client.login(config.servers[i].token);
 }
 
-// ---- Total Pop Bot ----
-const client = new Discord.Client({
+//Connecting to total count bot
+const totalclient = new Discord.Client({
     intents: [], presence: {
         status: "idle",
         activities: [{
@@ -85,21 +33,74 @@ const client = new Discord.Client({
         }]
     }
 });
-
-client.on('ready', () => {
-    console.log(`Connected to ${client.user.tag}`);
+totalclient.on('ready', () => {
+    console.log(`Connected to ${totalclient.user.tag}`);
 })
+totalclient.login(config.global_pop.token);
 
-setInterval(() => {
-    if (totalPlayers.length > 0) {
-        client.user.setPresence({
-            status: "online",
-            "activities": [{
-                name: `${totalPlayers.reduce((a, b) => a + b, 0)} Total Players`,
-                type: "PLAYING"
-            }]
-        });
+//refreshing player count
+setInterval(async () => {
+    const servers = await FetchServers();
+    let totalplayers = 0;
+
+    for (let i = 0; i < servers.length; i++) {
+        if (servers[i]['data']['attributes']['status'] === "offline" || servers[i]['data']['attributes']['status'] === "dead") {
+            clients[i].user.setPresence({
+                status: "dnd",
+                "activities": [{
+                    name: "Server Offline",
+                    type: "PLAYING"
+                }]
+            });
+        }
+        else if (servers[i]['data']['attributes']['details']['rust_queued_players']) {
+            clients[i].user.setPresence({
+                status: "online",
+                "activities": [{
+                    name: `${servers[i]['data']['attributes']['players']} / ${servers[i]['data']['attributes']['maxPlayers']}`,
+                    type: "PLAYING"
+                }]
+            });
+
+            totalplayers += servers[i]['data']['attributes']['players'];
+        }
+        else {
+            if (servers[i]['data']['attributes']['players'] < config.servers[i].custom_text_when_pop_below) {
+                clients[i].user.setPresence({
+                    status: "online",
+                    "activities": [{
+                        name: config.servers[i].custom_text,
+                        type: "PLAYING"
+                    }]
+                });
+            }
+            else {
+                clients[i].user.setPresence({
+                    status: "online",
+                    "activities": [{
+                        name: `${servers[i]['data']['attributes']['players']} / ${servers[i]['data']['attributes']['maxPlayers']} (${servers[i]['data']['attributes']['details']['rust_queued_players']})`,
+                        type: "PLAYING"
+                    }]
+                });
+            }
+
+            totalplayers += servers[i]['data']['attributes']['players'];
+        }
     }
-}, config.refresh_interval);
 
-client.login(config.global_pop.token);
+    totalclient.user.setPresence({
+        status: "online",
+        "activities": [{
+            name: `${totalplayers} Total Players`,
+            type: "PLAYING"
+        }]
+    });
+}, 10000);
+
+//function to fetch all servers
+async function FetchServers() {
+    const requests = config.servers.map((server) => fetch(`https://api.battlemetrics.com/servers/${server.battlemetrics_serverid}`));
+    const responses = await Promise.all(requests);
+    const promises = responses.map((response) => response.json());
+    return await Promise.all(promises);
+}
